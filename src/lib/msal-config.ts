b3bc0a655f1999545c2531583737c20c61d5ea43
Abis Ali <95873631+abisalisherkot@users.noreply.github.com>
2025-10-19 16:06:18 +0500

    fixes

--- src/lib/msal-config.ts
+++ src/lib/msal-config.ts
@@ -4,10 +4,8 @@ import { Configuration, PopupRequest } from "@azure/msal-browser";
 const isProd = typeof window !== "undefined" &&
   window.location.origin.includes("azurestaticapps.net");
 
-const redirectUri = isProd
-  ? "https://white-wave-0c4e4e61e.3.azurestaticapps.net/"
-  : "http://localhost:3000";
-
+const redirectUri = "https://white-wave-0c4e4e61e.3.azurestaticapps.net/"
+  
 export const msalConfig: Configuration = {
   auth: {
     clientId: "190c5bd6-ab69-4f8b-9165-4fca9c21c743",
